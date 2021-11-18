d96b0c80fc31e30f9c0e7f9b8dcdc960161746af
jerome-wy <jeromedaniel.wy@gmail.com>
2021-11-18 14:12:35 -0800

    Added the extra pages. Basic CSS completed. Working on JS next

---
+++ app.js
@@ -0,0 +1,11 @@
+// ------ VARIABLES
+let startBtn = document.getElementById('startGameBtn');
+let instBtn = document.getElementById('instructionGameBtn');
+let chngModeBtn = document.getElementById('changeModeBtn');
+let chngModeBtn = document.getElementById('difficultyBtn');
+
+// ------ FUNCTIONS
+
+
+
+// ------ EVENT LISTENERS
