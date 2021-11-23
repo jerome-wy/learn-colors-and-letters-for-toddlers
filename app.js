c4762d512fa7190c0fd1ed7e15d4777df20d9303
jerome-wy <jeromedaniel.wy@gmail.com>
2021-11-22 18:38:09 -0800

    Project mostly completed. Few touchups left

--- app.js
+++ app.js
@@ -2,22 +2,26 @@
 let startBtn = document.getElementById('startGameBtn');
 let instBtn = document.getElementById('instructionGameBtn');
 let darkModeBtn = document.getElementById('darkModeBtn');
-let changeDiffBtn = document.getElementById('changeDiffBtn');
+let changeModeBtn = document.getElementById('changeModeBtn');
 let lettersBtn = document.getElementById('lettersBtn');
 let wordsBtn = document.getElementById('wordsBtn');
 let colorText = document.querySelector('.colorText');
 let addWordBtn = document.getElementById('addWordBtn');
 let userInputForm = document.getElementById('userInputForm');
 let pushTextBtn = document.getElementById('pushTextBtn');
+let nextLetterIndexBtn = document.getElementById('nextLetterIndexBtn');
+let nextNumberIndexBtn = document.getElementById('nextNumberIndexBtn');
+let nextWordIndexBtn = document.getElementById('nextWordIndexBtn');
 let squares = document.querySelectorAll('.colorSquare');
 let textGenerator = document.getElementById('textGeneratorId');
-let textGeneratorColor = document.getElementById('textGeneratorId').style.color;
 let questionGenerator1 = document.getElementById('questionGenerator1');
 let questionGenerator2 = document.getElementById('questionGenerator2');
+let welcomeText = document.getElementById('welcomeText');
 let answer = document.getElementById('answer');
 let gameAlive = true;
 let next = true;
 let score = 0;
+let scoreDisplay = document.getElementById('scoreDisplay');
 
 // -----------------------------------------
 let squareColor = [
@@ -27,15 +31,6 @@ let squareColor = [
   'rgb(13, 13, 223)', //Blue
   'rgb(255, 246, 1)', //Yellow
   'rgb(254, 0, 0)' //Red
-];
-
-let squareFunColor = [
-  'rgb(52, 38, 75)', //Purple
-  'rgb(32, 83, 45)', //Green
-  'rgb(109, 74, 44)', //Orange
-  'rgb(32, 32, 92)', //Blue
-  'rgb(90, 88, 26)', //Yellow
-  'rgb(100, 27, 27)' //Red
 ];
 
 // ------- ARRAY VARIABLES - EASY MODE -----------
@@ -69,16 +64,12 @@ let lettersArray = [
 ];
 
 let numbersArray = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-  41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 100, 101, 102, 103, 104, 105, 106,
-  107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
-  122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136,
-  137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151,
-  152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166,
-  167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181,
-  182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196,
-  197, 198, 199, 200
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+  80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
+  99, 100
 ];
 
 // ----------- ARRAY VARIABLES - ADVANCED MODE -----------
@@ -144,6 +135,10 @@ function hideEverything() {
   questionGenerator1.style.display = 'none';
   questionGenerator2.style.display = 'none';
   answer.style.display = 'none;';
+  scoreDisplay.style.display = 'none';
+  nextLetterIndexBtn.style.display = 'none';
+  nextNumberIndexBtn.style.display = 'none';
+  nextWordIndexBtn.style.display = 'none';
 }
 
 function showEverything() {
@@ -156,52 +151,127 @@ function showEverything() {
   answer.style.display = 'inline-block';
 }
 
+function showWordsEverything() {
+  colorText.style.display = 'inline-block';
+  textGenerator.style.display = 'inline-block';
+  questionGenerator1.style.display = 'inline-block';
+  questionGenerator2.style.display = 'inline-block';
+  questionGenerator1.innerText = 'What';
+  questionGenerator2.innerText = 'is it?';
+  answer.style.display = 'inline-block';
+}
 function hideGameButtons() {
   lettersBtn.style.display = 'none';
   numbersBtn.style.display = 'none';
   wordsBtn.style.display = 'none';
 }
+
+function hideNextButtons() {
+  nextLetterIndexBtn.style.display = 'none';
+  nextNumberIndexBtn.style.display = 'none';
+  nextWordIndexBtn.style.display = 'none';
+}
 //-----------------------------------------
 
 //---- FUNCTIONS TO SHOW GENERATED TEXT -----
 function generateLetters() {
-  showEverything();
+  letterRandomIndex = [Math.floor(Math.random() * lettersArray.length)];
+  randomLetter = lettersArray[letterRandomIndex];
+  squareRandomIndex = [Math.floor(Math.random() * squareColor.length)];
+  randomSquare = squareColor[squareRandomIndex];
   textGenerator.innerText = randomLetter;
   textGenerator.style.color = randomSquare;
+  showEverything();
 }
 
 function generateNumbers() {
+  numberRandomIndex = [Math.floor(Math.random() * numbersArray.length)];
+  randomNumber = numbersArray[numberRandomIndex];
+  squareRandomIndex = [Math.floor(Math.random() * squareColor.length)];
+  randomSquare = squareColor[squareRandomIndex];
   textGenerator.innerText = randomNumber;
   textGenerator.style.color = randomSquare;
   showEverything();
 }
 
 function generateWords() {
+  wordsRandomIndex = [Math.floor(Math.random() * wordsArray.length)];
+  randomWord = wordsArray[wordsRandomIndex];
   textGenerator.innerText = randomWord;
   textGenerator.style.color = randomSquare;
   showEverything();
 }
 
-function validateGame() {
+function validateLettersGame() {
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = squareColor[i];
     squares[i].addEventListener('click', function () {
       if (squareColor[i] === textGenerator.style.color) {
         squares[i].style.color = squareColor[i];
         answer.innerText = 'You are correct!';
+        // score += parseInt(score + 1);
+        scoreDisplay.style.display = 'inline-block';
+        scoreDisplay.innerText = 'SCORE: ' + score;
+        nextLetterIndexBtn.style.display = 'inline-block';
         gameAlive = false;
-      } else squareColor[i] != textGenerator.style.color, (answer.innerText = 'Try again!');
-      gameAlive = true;
+      }
+      if (squareColor[i] != textGenerator.style.color) {
+        answer.innerText = 'Try again!';
+        gameAlive = true;
+      }
     });
   }
 }
+
+function validateNumbersGame() {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = squareColor[i];
+    squares[i].addEventListener('click', function () {
+      if (squareColor[i] === textGenerator.style.color) {
+        squares[i].style.color = squareColor[i];
+        answer.innerText = 'You are correct!';
+        // score += parseInt(score + 1);
+        scoreDisplay.style.display = 'inline-block';
+        scoreDisplay.innerText = 'SCORE: ' + score;
+        nextNumberIndexBtn.style.display = 'inline-block';
+        gameAlive = false;
+      }
+      if (squareColor[i] != textGenerator.style.color) {
+        answer.innerText = 'Try again!';
+        gameAlive = true;
+      }
+    });
+  }
+}
+function validateWordsGame() {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = squareColor[i];
+    squares[i].addEventListener('click', function () {
+      if (squareColor[i] === textGenerator.style.color) {
+        squares[i].style.color = squareColor[i];
+        answer.innerText = 'You are correct!';
+        // score += parseInt(score + 1);
+        scoreDisplay.style.display = 'inline-block';
+        scoreDisplay.innerText = 'SCORE: ' + score;
+        nextWordIndexBtn.style.display = 'inline-block';
+        gameAlive = false;
+      }
+      if (squareColor[i] != textGenerator.style.color) {
+        answer.innerText = 'Try again!';
+        gameAlive = true;
+      }
+    });
+  }
+}
+//declare variable in if statement above for "match" - use switch case to match bgcolor -- put rgb in the switch case
+//
 
 //-----------------------------------------
 
 //---- FUNCTIONS GENERATE LETTER/NUMBER -----
 function startLettersGame() {
   generateLetters();
-  validateGame();
+  validateLettersGame();
 }
 
 // need a better way to check 'winner function'
@@ -210,74 +280,87 @@ function startLettersGame() {
 
 function startNumbersGame() {
   generateNumbers();
-  validateGame();
+  validateNumbersGame();
 }
 
 function startWordsGame() {
   generateWords();
-  validateGame();
+  validateWordsGame();
 }
 
 //------------------------------, ,-----------
 
 //---- EVENT LISTENERS----------------------
-changeDiffBtn.addEventListener('click', function () {
-  hideEverything();
+changeModeBtn.addEventListener('click', function () {
+  // hideEverything();
   lettersBtn.style.display = 'inline-block';
   numbersBtn.style.display = 'inline-block';
   wordsBtn.style.display = 'inline-block';
+  welcomeText.style.display = 'none';
+  answer.style.display = 'none';
 });
 
 lettersBtn.addEventListener('click', function () {
+  textGenerator.style.display = 'none';
   startLettersGame();
   document.querySelector('.allColorSquares').style.display = 'inline-block';
   hideGameButtons();
-  if (hideGameButtons == true) {
-    generateLetters();
-  }
+  hideNextButtons();
 });
 
 numbersBtn.addEventListener('click', function () {
   startNumbersGame();
   hideGameButtons();
   document.querySelector('.allColorSquares').style.display = 'inline-block';
+  hideNextButtons();
 });
 
 wordsBtn.addEventListener('click', function () {
   startWordsGame();
   hideGameButtons();
   document.querySelector('.allColorSquares').style.display = 'inline-block';
+  hideNextButtons();
 });
 
-//---- CREATE A NEW PAGE SPECIFICALLY FOR NUMBERS
-// advancedBtn.addEventListener('click', function () {
-//   startAdvNumbersGame();
-//   easyBtn.style.display = 'none';
-//   mediumBtn.style.display = 'none';
-//   advancedBtn.style.display = 'none';
-//   document.querySelector('.allColorSquares').style.display = 'inline-block';
-// });
+nextLetterIndexBtn.addEventListener('click', function () {
+  answer.innerText = '';
+  generateLetters();
+  hideNextButtons();
+});
+
+nextNumberIndexBtn.addEventListener('click', function () {
+  answer.innerText = '';
+  generateNumbers();
+  hideNextButtons();
+});
+
+nextWordIndexBtn.addEventListener('click', function () {
+  answer.innerText = '';
+  generateWords();
+  hideNextButtons();
+});
 // ---------------------------------------
 
 // ADD WORD TO ARRAY --------------------
 addWordBtn.addEventListener('click', function () {
   hideGameButtons();
-  startWordsGame();
-  showEverything();
-  validateGame();
+  validateWordsGame();
+  generateWords();
   document.querySelector('.allColorSquares').style.display = 'inline-block';
+  welcomeText.style.display = 'none';
   userInputForm.style.display = 'inline-block';
   pushTextBtn.style.display = 'inline-block';
   pushTextBtn.addEventListener('click', function () {
     userInputForm.style.display = 'none';
     pushTextBtn.style.display = 'none';
-    // generateAdvancedLetters();
-    return wordsArray.unshift(userInputForm.value);
+    wordsArray.unshift(userInputForm.value);
+    textGenerator.innerText = wordsArray[0];
   });
 });
 
 darkModeBtn.addEventListener('click', function () {
   document.body.classList.toggle('darkMode');
+  welcomeText.style.display = 'none';
 });
 // ----- CALL DEFAULT FUNCTIONS -----
 hideEverything();
